41c3133deed20cdabad69368dd64c6b789ab923e
Lisa Stephens <lisakstep@gmail.com>
2014-05-28 16:14:14 -0600

    improved input validation and cleaned up/improved comments

--- PlaySpace/src/CheckForPrimeness.java
+++ PlaySpace/src/CheckForPrimeness.java
@@ -1,35 +1,57 @@
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
+ * @author Lisa Stephens
+ * completed May 28, 2014
  * 
- */
-
-/**
- * @author lisastephens
- * determine if a number is prime
+ * determine if an integer is prime
+ * 
+ * This method will work for valid Java ints, that is integers
+ * from -2 to the 31st power to (2 to the 31st power -1 )
  *
  */
 public class CheckForPrimeness {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// main class for using the checkIsPrime method
+		
+		// get the number to which to apply the primeness test from user input
 		int numberToTest = getTheUserNumber();
+		// test the number
 		Boolean numberIsPrime = checkIsPrime(numberToTest);
 
-		System.out.println("Testing " + numberToTest);
-
+		// print out appropriate response
 		if (numberIsPrime ) {
 			System.out.println(numberToTest + " is prime.");
 		}
 		else {
 			System.out.println(numberToTest + " is composite (not prime).");
-
 		}
+	}
 
+
+	private static int getTheUserNumber() {
+		// get the number from the user. ensure it is an integer, return the integer
+		Scanner scanner = new Scanner(System.in);
+
+		int numberToTest = 0;
+		Boolean validInput = false;
+
+		// cycle through the input process until an integer is received
+		while (!validInput) {
+			System.out.println("Enter the number to be tested for prime-ness: ");
+			if ( !scanner.hasNextInt()) {
+				System.out.println("Please enter an integer.");
+				scanner.next();
+			}
+			else {
+				numberToTest = scanner.nextInt();
+				validInput = true;
+			}	
+		}
+		// return the integer to the caller
+		return numberToTest;
 	}
 
 	private static Boolean checkIsPrime(int numberToTest) {
@@ -37,6 +59,7 @@ public class CheckForPrimeness {
 		 * This java method takes an integer as an input and returns a boolean value indicating if the
 		 * number is prime (true) or not (false).
 		 */
+		
 		// By definition, 1 and 2 are prime and any integer less than 1 is not
 		if ( ((numberToTest) == 1) || (numberToTest == 2) ) {
 			return true;
@@ -44,6 +67,7 @@ public class CheckForPrimeness {
 		if (numberToTest < 1) {
 			return false;
 		}
+		
 		// check primeness for integers with value greater than 2
 		for (int i=2; i< (numberToTest-1) ; i++ ) {
 			if (((numberToTest % i) == 0) ) {
@@ -51,27 +75,6 @@ public class CheckForPrimeness {
 			}
 		}
 		return true;
-	}
-
-	private static int getTheUserNumber() {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(new InputStreamReader(System.in));
-
-		int numberToTest = 0;
-		Boolean validInput = false;
-
-		while (!validInput) {
-
-			System.out.println("Enter the number to be tested for prime-ness: ");
-			numberToTest = scanner.nextInt();
-			if (numberToTest < 1) {
-				System.out.println("Please enter an integer greater than 0.");
-			}
-			else {
-				validInput = true;
-			}
-		}
-		return numberToTest;
 	}
 
 }
